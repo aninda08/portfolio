aa0330ea5edc92edef36acfc93a85011c139bbe8
Aninda Chakraborty <aninda.death@gmail.com>
2021-05-29 23:10:00 +0530

    Setting up unicorn eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,54 @@
 module.exports = {
   plugins: ['prettier'],
-  extends: ['airbnb-typescript', 'airbnb/hooks', 'prettier'],
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'prettier',
+    'plugin:unicorn/recommended',
+  ],
   rules: {
+    'import/no-namespace': 'error',
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'unicorn/filename-case': [
+      'error',
+      {
+        cases: {
+          camelCase: true,
+          pascalCase: true,
+        },
+      },
+    ],
+    'unicorn/no-array-for-each': 'off', //better to turn on for strict linting
+    'unicorn/no-array-reduce': 'off',
+    'unicorn/prevent-abbreviations': 'off', //better to turn on for strict linting
+    'unicorn/no-null': 'off',
+    'unicorn/prefer-query-selector': 'off',
   },
   parserOptions: {
     project: './tsconfig.json',
   },
+  overrides: [
+    {
+      files: ['src/components/**/*.tsx'],
+      rules: {
+        'unicorn/filename-case': [
+          'error',
+          {
+            case: 'pascalCase',
+          },
+        ],
+      },
+    },
+    {
+      files: ['src/pages/**/*.tsx'],
+      rules: {
+        'unicorn/filename-case': [
+          'error',
+          {
+            case: 'kebabCase',
+          },
+        ],
+      },
+    },
+  ],
 };
---
+++ src/constants/application.ts
@@ -0,0 +1,2 @@
+export const APP_NAME = 'Portfolio';
+export const VERSION = '1.0.0';
